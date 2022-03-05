b79448ac627c278a4f4d3b44bcff7e33721517e9
Brunner <lukasbrunnerjku@gmx.at>
2022-03-05 17:25:31 +0100

    added coupling layer

--- nf.py
+++ nf.py
@@ -9,8 +9,111 @@ import pytest
 import pdb
 
 
+class FastFlowCoupling(nn.Module):
+    def __init__(
+        self,
+        channels: int,
+        subnet_constructor: callable = None,
+        clamp_activation: str = "ATAN",
+        clamp: float = 2.0,
+    ) -> None:
+        super().__init__()
+
+        assert channels % 2 == 0
+
+        self.subnet1 = subnet_constructor(channels // 2, channels)
+        self.subnet2 = subnet_constructor(channels // 2, channels)
+        self.channels = channels
+        self.clamp = clamp
+
+        if clamp_activation == "ATAN":
+            self.f_clamp = lambda u: 0.636 * torch.atan(u)
+        elif clamp_activation == "TANH":
+            self.f_clamp = torch.tanh
+        elif clamp_activation == "SIGMOID":
+            self.f_clamp = lambda u: 2.0 * (torch.sigmoid(u) - 0.5)
+        else:
+            raise ValueError(f'Unknown clamp activation "{clamp_activation}"')
+
+    def forward(self, x, rev=False):
+        x1, x2 = torch.split(x, self.channels // 2, dim=1)
+
+        if rev:
+            pass
+        else:
+            o1 = self.subnet1(x1)
+            s1, b1 = o1[:, : self.channels // 2], o1[:, self.channels // 2 :]
+            s1 = torch.exp(self.clamp * self.f_clamp(s1))
+            y2 = s1 * x2 + b1
+
+            o2 = self.subnet2(y2)
+            s2, b2 = o2[:, : self.channels // 2], o2[:, self.channels // 2 :]
+            s2 = torch.exp(self.clamp * self.f_clamp(s2))
+            y1 = s2 * x1 + b2
+
+            s = torch.cat((s2, s1), dim=1)
+            logdet = torch.sum(torch.log(s), dim=tuple(range(1, s.ndim)))  # B,
+            y = torch.cat((y1, y2), dim=1)
+
+        return y, logdet
+
+
+def test_coupling(channels=8):
+    def linear(in_channels, out_channels, hidden_channels=64):
+        return nn.Sequential(
+            nn.Linear(in_channels, hidden_channels, bias=True),
+            nn.ReLU(inplace=True),
+            nn.Linear(hidden_channels, out_channels, bias=True),
+        )
+
+    def conv1d(in_channels, out_channels, hidden_channels=64):
+        return nn.Sequential(
+            nn.Conv1d(in_channels, hidden_channels, 3, 1, 1, bias=True),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(hidden_channels, out_channels, 3, 1, 1, bias=True),
+        )
+
+    def conv2d(in_channels, out_channels, hidden_channels=64):
+        return nn.Sequential(
+            nn.Conv2d(in_channels, hidden_channels, 3, 1, 1, bias=True),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(hidden_channels, out_channels, 3, 1, 1, bias=True),
+        )
+
+    def conv3d(in_channels, out_channels, hidden_channels=64):
+        return nn.Sequential(
+            nn.Conv3d(in_channels, hidden_channels, 3, 1, 1, bias=True),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(hidden_channels, out_channels, 3, 1, 1, bias=True),
+        )
+
+    couple = FastFlowCoupling(channels, linear)
+    x = torch.randn(1, channels)
+    y, logdet = couple(x)
+    assert y.shape == x.shape
+    assert logdet.shape == x.shape[:1]
+
+    couple = FastFlowCoupling(channels, conv1d)
+    x = torch.randn(1, channels, 5)
+    y, logdet = couple(x)
+    assert y.shape == x.shape
+    assert logdet.shape == x.shape[:1]
+
+    couple = FastFlowCoupling(channels, conv2d)
+    x = torch.randn(1, channels, 5, 5)
+    y, logdet = couple(x)
+    assert y.shape == x.shape
+    assert logdet.shape == x.shape[:1]
+
+    couple = FastFlowCoupling(channels, conv3d)
+    x = torch.randn(1, channels, 5, 5, 5)
+    y, logdet = couple(x)
+    assert y.shape == x.shape
+    assert logdet.shape == x.shape[:1]
+
+
 class Permute(nn.Module):
-    def __init__(self, channels, ndim: int = 0) -> None:
+    def __init__(self, channels: int, ndim: int = 0) -> None:
         super().__init__()
 
         w = np.zeros((channels, channels))
@@ -61,4 +164,6 @@ def test_permute(channels=8):
 
 if __name__ == "__main__":
     test_permute(channels=8)
+    test_coupling(channels=8)
+
     # pdb.set_trace()
