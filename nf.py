9ad64a9eaa121ff5bd17e701776109fd8e3d9ecc
Brunner <lukasbrunnerjku@gmx.at>
2022-03-06 17:12:59 +0100

    finished module implementation with minor stability issue

--- nf.py
+++ nf.py
@@ -1,3 +1,4 @@
+from subprocess import call
 import numpy as np
 import torch
 import torchvision as tv
@@ -46,18 +47,48 @@ class ActNorm(nn.Module):
         if rev:
             y = (x - self.global_offset) / s
             logdet = -torch.sum(
-                torch.log(torch.abs(self.s)), dim=tuple(range(1, s.ndim))
+                torch.log(torch.abs(s)), dim=tuple(range(1, s.ndim))
             )  # B,
         else:
             y = s * x + self.global_offset
             logdet = torch.sum(
-                torch.log(torch.abs(self.s)), dim=tuple(range(1, s.ndim))
+                torch.log(torch.abs(s)), dim=tuple(range(1, s.ndim))
             )  # B,
 
         return y, logdet
 
 
-class FastFlowCoupling(nn.Module):
+def test_norm(channels=8):
+    actnorm = ActNorm(channels, 0)
+    x = torch.randn(1, channels)
+    y, logdet = actnorm(x)
+    assert y.shape == x.shape
+    assert logdet.shape == x.shape[:1]
+    assert torch.allclose(actnorm(y, rev=True)[0], x)
+
+    actnorm = ActNorm(channels, 1)
+    x = torch.randn(1, channels, 5)
+    y, logdet = actnorm(x)
+    assert y.shape == x.shape
+    assert logdet.shape == x.shape[:1]
+    assert torch.allclose(actnorm(y, rev=True)[0], x)
+
+    actnorm = ActNorm(channels, 2)
+    x = torch.randn(1, channels, 5, 5)
+    y, logdet = actnorm(x)
+    assert y.shape == x.shape
+    assert logdet.shape == x.shape[:1]
+    assert torch.allclose(actnorm(y, rev=True)[0], x)
+
+    actnorm = ActNorm(channels, 3)
+    x = torch.randn(1, channels, 5, 5, 5)
+    y, logdet = actnorm(x)
+    assert y.shape == x.shape
+    assert logdet.shape == x.shape[:1]
+    assert torch.allclose(actnorm(y, rev=True)[0], x)
+
+
+class Coupling(nn.Module):
     def __init__(
         self,
         channels: int,
@@ -84,11 +115,28 @@ class FastFlowCoupling(nn.Module):
             raise ValueError(f'Unknown clamp activation "{clamp_activation}"')
 
     def forward(self, x, rev=False):
-        x1, x2 = torch.split(x, self.channels // 2, dim=1)
-
         if rev:
-            pass
+            y1, y2 = torch.split(x, self.channels // 2, dim=1)
+
+            o2 = self.subnet2(y2)
+            s2, b2 = o2[:, : self.channels // 2], o2[:, self.channels // 2 :]
+            s2 = torch.exp(self.clamp * self.f_clamp(s2))
+            x1 = (y1 - b2) / s2
+
+            o1 = self.subnet1(x1)
+            s1, b1 = o1[:, : self.channels // 2], o1[:, self.channels // 2 :]
+            s1 = torch.exp(self.clamp * self.f_clamp(s1))
+            x2 = (y2 - b1) / s1
+
+            s = torch.cat((s2, s1), dim=1)
+            logdet = -torch.sum(torch.log(s), dim=tuple(range(1, s.ndim)))  # B,
+            x = torch.cat((x1, x2), dim=1)
+
+            return x, logdet
+
         else:
+            x1, x2 = torch.split(x, self.channels // 2, dim=1)
+
             o1 = self.subnet1(x1)
             s1, b1 = o1[:, : self.channels // 2], o1[:, self.channels // 2 :]
             s1 = torch.exp(self.clamp * self.f_clamp(s1))
@@ -103,87 +151,70 @@ class FastFlowCoupling(nn.Module):
             logdet = torch.sum(torch.log(s), dim=tuple(range(1, s.ndim)))  # B,
             y = torch.cat((y1, y2), dim=1)
 
-        return y, logdet
+            return y, logdet
 
 
-def test_norm(channels=8):
-    actnorm = ActNorm(channels, 0)
-    x = torch.randn(1, channels)
-    y, logdet = actnorm(x)
-    assert y.shape == x.shape
-    assert logdet.shape == x.shape[:1]
+def linear(in_channels, out_channels, hidden_channels=64):
+    return nn.Sequential(
+        nn.Linear(in_channels, hidden_channels, bias=True),
+        nn.ReLU(inplace=True),
+        nn.Linear(hidden_channels, out_channels, bias=True),
+    )
 
-    actnorm = ActNorm(channels, 1, 5)
-    x = torch.randn(1, channels)
-    y, logdet = actnorm(x)
-    assert y.shape == x.shape
-    assert logdet.shape == x.shape[:1]
 
-    actnorm = ActNorm(channels, 2)
-    x = torch.randn(1, channels, 5, 5)
-    y, logdet = actnorm(x)
-    assert y.shape == x.shape
-    assert logdet.shape == x.shape[:1]
+def conv1d(in_channels, out_channels, hidden_channels=64):
+    return nn.Sequential(
+        nn.Conv1d(in_channels, hidden_channels, 3, 1, 1, bias=True),
+        nn.ReLU(inplace=True),
+        nn.Conv1d(hidden_channels, out_channels, 3, 1, 1, bias=True),
+    )
 
-    actnorm = ActNorm(channels, 3)
-    x = torch.randn(1, channels, 5, 5, 5)
-    y, logdet = actnorm(x)
-    assert y.shape == x.shape
-    assert logdet.shape == x.shape[:1]
+
+def conv2d(in_channels, out_channels, hidden_channels=64):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, hidden_channels, 3, 1, 1, bias=True),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(hidden_channels, out_channels, 3, 1, 1, bias=True),
+    )
+
+
+def conv3d(in_channels, out_channels, hidden_channels=64):
+    return nn.Sequential(
+        nn.Conv3d(in_channels, hidden_channels, 3, 1, 1, bias=True),
+        nn.ReLU(inplace=True),
+        nn.Conv3d(hidden_channels, out_channels, 3, 1, 1, bias=True),
+    )
 
 
 def test_coupling(channels=8):
-    def linear(in_channels, out_channels, hidden_channels=64):
-        return nn.Sequential(
-            nn.Linear(in_channels, hidden_channels, bias=True),
-            nn.ReLU(inplace=True),
-            nn.Linear(hidden_channels, out_channels, bias=True),
-        )
 
-    def conv1d(in_channels, out_channels, hidden_channels=64):
-        return nn.Sequential(
-            nn.Conv1d(in_channels, hidden_channels, 3, 1, 1, bias=True),
-            nn.ReLU(inplace=True),
-            nn.Conv1d(hidden_channels, out_channels, 3, 1, 1, bias=True),
-        )
-
-    def conv2d(in_channels, out_channels, hidden_channels=64):
-        return nn.Sequential(
-            nn.Conv2d(in_channels, hidden_channels, 3, 1, 1, bias=True),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(hidden_channels, out_channels, 3, 1, 1, bias=True),
-        )
-
-    def conv3d(in_channels, out_channels, hidden_channels=64):
-        return nn.Sequential(
-            nn.Conv3d(in_channels, hidden_channels, 3, 1, 1, bias=True),
-            nn.ReLU(inplace=True),
-            nn.Conv3d(hidden_channels, out_channels, 3, 1, 1, bias=True),
-        )
-
-    couple = FastFlowCoupling(channels, linear)
+    couple = Coupling(channels, linear)
     x = torch.randn(1, channels)
     y, logdet = couple(x)
     assert y.shape == x.shape
     assert logdet.shape == x.shape[:1]
+    assert torch.allclose(couple(y, rev=True)[0], x, atol=1e-7)
 
-    couple = FastFlowCoupling(channels, conv1d)
+    couple = Coupling(channels, conv1d)
     x = torch.randn(1, channels, 5)
     y, logdet = couple(x)
     assert y.shape == x.shape
     assert logdet.shape == x.shape[:1]
+    assert torch.allclose(couple(y, rev=True)[0], x, atol=1e-7)
 
-    couple = FastFlowCoupling(channels, conv2d)
+    couple = Coupling(channels, conv2d)
     x = torch.randn(1, channels, 5, 5)
     y, logdet = couple(x)
     assert y.shape == x.shape
     assert logdet.shape == x.shape[:1]
+    assert torch.allclose(couple(y, rev=True)[0], x, atol=1e-7)
 
-    couple = FastFlowCoupling(channels, conv3d)
+    couple = Coupling(channels, conv3d)
     x = torch.randn(1, channels, 5, 5, 5)
     y, logdet = couple(x)
     assert y.shape == x.shape
     assert logdet.shape == x.shape[:1]
+    assert torch.allclose(couple(y, rev=True)[0], x, atol=1e-7)
 
 
 class Permute(nn.Module):
@@ -236,8 +267,138 @@ def test_permute(channels=8):
     assert torch.allclose(perm(y, rev=True)[0], x)
 
 
-if __name__ == "__main__":
-    test_permute(channels=8)
-    test_coupling(channels=8)
+def subnet_conv_1x1(in_channels, out_channels, hidden_channels=128):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, hidden_channels, kernel_size=1, padding="same"),
+        nn.ReLU(),
+        nn.Conv2d(hidden_channels, out_channels, kernel_size=1, padding="same"),
+    )
 
-    # pdb.set_trace()
+
+def subnet_conv_3x3(in_channels, out_channels, hidden_channels=128):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, hidden_channels, kernel_size=3, padding="same"),
+        nn.ReLU(),
+        nn.Conv2d(hidden_channels, out_channels, kernel_size=3, padding="same"),
+    )
+
+
+class FlowSequential(nn.Sequential):
+    def __init__(self, *args: nn.Module):
+        super().__init__(*args)
+
+    def forward(self, x, rev=False):
+        sumlogdet = 0.0
+        modules = reversed([m for m in iter(self)]) if rev else self
+        for module in modules:
+            x, logdet = module(x, rev)
+            sumlogdet += logdet
+        return x, sumlogdet
+
+
+def test_flowsequential(channels=8):
+
+    seq = FlowSequential(
+        ActNorm(channels, 0),
+        Permute(channels, 0),
+        Coupling(channels, linear),
+        ActNorm(channels, 0),
+        Permute(channels, 0),
+        Coupling(channels, linear),
+    )
+    x = torch.randn(1, channels)
+    y, sumlogdet = seq(x)
+    x_rev, sumlogdet_rev = seq(y, rev=True)
+    assert torch.allclose(x_rev, x, atol=1e-7)
+    assert torch.allclose(sumlogdet_rev, -sumlogdet, atol=1e-7)
+
+    seq = FlowSequential(
+        ActNorm(channels, 1),
+        Permute(channels, 1),
+        Coupling(channels, conv1d),
+        ActNorm(channels, 1),
+        Permute(channels, 1),
+        Coupling(channels, conv1d),
+    )
+    x = torch.randn(1, channels, 5)
+    y, sumlogdet = seq(x)
+    x_rev, sumlogdet_rev = seq(y, rev=True)
+    assert torch.allclose(x_rev, x, atol=1e-7)
+    assert torch.allclose(sumlogdet_rev, -sumlogdet, atol=1e-7)
+
+    seq = FlowSequential(
+        ActNorm(channels, 2),
+        Permute(channels, 2),
+        Coupling(channels, conv2d),
+        ActNorm(channels, 2),
+        Permute(channels, 2),
+        Coupling(channels, conv2d),
+    )
+    x = torch.randn(1, channels, 5, 5)
+    y, sumlogdet = seq(x)
+    x_rev, sumlogdet_rev = seq(y, rev=True)
+    assert torch.allclose(x_rev, x, atol=1e-7)
+    assert torch.allclose(sumlogdet_rev, -sumlogdet, atol=1e-7)
+
+    seq = FlowSequential(
+        ActNorm(channels, 3),
+        Permute(channels, 3),
+        Coupling(channels, conv3d),
+        ActNorm(channels, 3),
+        Permute(channels, 3),
+        Coupling(channels, conv3d),
+    )
+    x = torch.randn(1, channels, 5, 5, 5)
+    y, sumlogdet = seq(x)
+    x_rev, sumlogdet_rev = seq(y, rev=True)
+    assert torch.allclose(x_rev, x, atol=1e-7)
+    assert torch.allclose(sumlogdet_rev, -sumlogdet, atol=1e-7)
+
+
+class FastFlow(nn.Module):
+    def __init__(
+        self,
+        channels: int,
+        ndim: int,
+        subnet_constructor1: callable,
+        subnet_constructor2: callable,
+        num_blocks: int = 8,
+    ) -> None:
+        super().__init__()
+
+        blocks = []
+        for k in range(num_blocks):
+            subnet_constructor = (
+                subnet_constructor1 if (k % 2 == 0) else subnet_constructor2
+            )
+            blocks.extend(
+                [
+                    ActNorm(channels, ndim),
+                    Permute(channels, ndim),
+                    Coupling(channels, subnet_constructor),
+                ]
+            )
+        self.blocks = FlowSequential(*blocks)
+
+    def forward(self, x, rev=False):
+        return self.blocks(x, rev)
+
+
+def test_fastflow(channels=8, num_blocks=4):
+
+    # TODO: numerical stability issues ie. with 8 blocks won't pass test!
+
+    fastflow = FastFlow(channels, 2, subnet_conv_3x3, subnet_conv_1x1, num_blocks)
+    x = torch.randn(1, channels, 5, 5)
+    y, sumlogdet = fastflow(x)
+    x_rev, sumlogdet_rev = fastflow(y, rev=True)
+    assert torch.allclose(x_rev, x, atol=1e-7)
+    assert torch.allclose(sumlogdet_rev, -sumlogdet, atol=1e-7)
+
+
+if __name__ == "__main__":
+    # test_permute(channels=8)
+    # test_coupling(channels=8)
+    # test_norm(channels=8)
+    # test_flowsequential(channels=8)
+    test_fastflow(channels=8)
